5326c1e94632141b60b6b94ceecdfdbde4c4c0a1
Your Name <idangi>
2024-03-22 01:34:17 +0200

    git initialize

---
+++ src/test/java/dev/givati/gymz/GymzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.givati.gymz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
